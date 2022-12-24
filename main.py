770df8aa50a72d280a43404236053e55837b8727
farrux1994 <f.turdiyevbek94@gmail.com>
2022-12-24 19:21:42 +0500

    main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 def main(S, d):
-    '''create a babylonian function.
-    
-    Args:
-        S (int): number
-        d (int): numnber
-        
-    Returns:
-        float: result
-    '''
-    return
+    a = (S-d*d)/(2*d)
+    b = a+d
+    x = (b-a*a)/(2*b)
+
+    return x
+
+k = main(12, 2)
+print(k)
